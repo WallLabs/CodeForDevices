5ede7ecb7b650822090bff7f27d32f9c676d4b2c
Tony Wall <tony@tonywall.com>
2018-07-24 11:20:16 +0200

    Create solution and build scripts.

---
+++ Source/Code for Devices on Windows/Common/AssemblyInfoGlobal.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+
+// Information
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Wall Labs")]
+[assembly: AssemblyProduct("Code for Windows on Devices")]
+[assembly: AssemblyCopyright("Copyright © Wall Labs")]
+
+// Version
+[assembly: AssemblyVersion("4.72.1807.24007")]
+[assembly: AssemblyFileVersion("4.72.1807.24007")]
